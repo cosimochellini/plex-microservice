8d68427a604be141c4bdcc855e11d8a7706279a6
Cosimo <cosimo.chellini@gmail.com>
2020-01-03 16:26:03 +0100

    init app

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,17 @@ export const expressApp = express();
 
 /* istanbul ignore next */
 expressApp.get('/', (req, res) => {
-  const response: HelloWorldResponse = { message: 'Hello World!' };
+  const value = req.query.value;
+
+  const response: HelloWorldResponse = { message: 'Hello World!' + value };
   res.send(response);
 });
 
 /**
- * Start the express application.
- *
- * @param port The port to listen to.
- */
+* Start the express application.
+*
+* @param port The port to listen to.
+*/
 export function start(port = 3000) {
   expressApp.listen(port, (err: any) => {
     if (err) {
