83663cd99f6a0994ea4741921fc6d3bfcd32c1a9
cosimo chellini <cosimo.chellini@gmail.com>
2021-05-08 11:10:16 +0200

    fix(dep): updated dependency

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,14 @@
-import { app, port } from '@/services/express.service';
-import { getMediaContent } from '@/controllers/v1/plex.controller';
+import { app, port } from "@/services/express.service";
+import { getMediaContent } from "@/controllers/v1/plex.controller";
 
-app.get('/v1/', getMediaContent);
-app.get('/health', (req, res) => res.send(true));
+app.get("/v1/", getMediaContent);
+app.get("/health", (req, res) => res.send(true));
 
 const start = () => {
-
-  app.listen(port, (err: any) => {
-    if (err) {
-      console.error(`Unable to start app. Found error: ${err.message}`);
-      return;
-    }
+  app.listen(port, () => {
     console.info(`Example app listening on port ${port}!`);
     return;
   });
 };
 
-export {
-  app,
-  start,
-};
+export { app, start };
